3bc1eb2ba51dd7be2201c824d81720dbc7502f2f
Michael Kreil <github@michael-kreil.de>
2016-02-17 20:19:16 +0100

    better play script

--- mateplay.sh
+++ mateplay.sh
@@ -1,1 +1,6 @@
-ffmpeg -re -i $1 -f rawvideo -vcodec rawvideo -pix_fmt rgb24 - > /dev/udp/matelight.cbrp3.c-base.org/1337
+echo "streaming '$1' to matelight. quit with ctrl-c."
+while [ 1 ]
+do
+	ffmpeg -loglevel error -re -i $1 -f rawvideo -vcodec rawvideo -pix_fmt rgb24 - > /dev/udp/matelight.cbrp3.c-base.org/1337
+	test $? -eq 255 && break
+done
